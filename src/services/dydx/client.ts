1a68683325779595d93651f024a76f561763e9b4
jpatidar30 <jpatidar@deqode.com>
2024-07-12 16:19:23 +0530

    fix: remove check for PERPETUAL_PRIVATE_KEY

--- src/services/dydx/client.ts
+++ src/services/dydx/client.ts
@@ -11,7 +11,6 @@ class DYDXConnector {
 		if (
 			!process.env.API_KEY ||
 			!process.env.API_PASSPHRASE ||
-			!process.env.PERPETUAL_PRIVATE_KEY ||
 			!process.env.API_SECRET ||
 			!process.env.AES_KEY
 		) {
